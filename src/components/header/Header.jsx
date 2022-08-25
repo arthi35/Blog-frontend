480dda001f186c45b481306610b283cdd8b108ec
arthi35 <arthimba35@gmail.com>
2022-08-25 13:24:44 +0530

    hello

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import "./header.css"
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">Node&Javascript</span>
+        <span className="headerTitleLg">Blog</span>
+      </div>
+      <img className="headerImg"src="https://cutewallpaper.org/23/technology-4k-wallpaper/2570811559.jpg" alt="" />
+    </div>
+  )
+}
---
+++ src/components/posts/Posts.jsx
@@ -0,0 +1,14 @@
+import Post from "../post/Post";
+import "./posts.css";
+
+export default function Posts() {
+  return (
+    <div className="posts">
+      <Post img="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
+      <Post img="https://images.unsplash.com/photo-1661097410573-16d926ec1ec7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0NHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" />
+      <Post img="https://images.pexels.com/photos/6711867/pexels-photo-6711867.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"/>
+      <Post img="https://images.pexels.com/photos/5490778/pexels-photo-5490778.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"/>
+      <Post img="https://images.pexels.com/photos/4916559/pexels-photo-4916559.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"/>
+    </div>
+  );
+}
